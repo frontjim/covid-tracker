c9c287deb5008827d13946a5917c688cd3f7c404
frontjim <frontjim@users.noreply.github.com>
2021-09-05 15:47:29 +0300

    v1

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,24 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <Header />
+  <router-view class="m-10"/>
+  <Footer class="m-10"/>
 </template>
 
+<script>
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
+export default {
+  components: {
+    Header,
+    Footer
+  }
+  
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap');
+* {
+  font-family: 'Poppins', sans-serif;
 }
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+	<footer class="text-center h-auto m-auto">
+		<div class="absolute bottom-0">
+			<p id="c">Kortsi &copy; 2021</p>
+			<p id="lov">Coded with &#10084;&#65039;</p>
+		</div>
+	</footer>
+</template>
+
+<style scoped>
+div{
+	transform: translateX(50vw);
+}
+#c{
+	font-size: 0.9em;
+}
+#lov{
+	font-size: 0.7em;
+}
+</style>
