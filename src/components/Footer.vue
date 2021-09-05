2f9ddd816896244653b857004a97d5d76f34c509
frontjim <frontjim@users.noreply.github.com>
2021-09-05 18:31:44 +0300

    footer fix

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template >
-	<footer class=" text-center flex align-items-center justify-content-center">
-		<div >
+	<footer>
+		<div class="text-center flex flex-col ">
 			<p id="c">Kortsi &copy; 2021</p>
 			<p id="lov">Coded with &#10084;&#65039;</p>
 		</div>
@@ -8,9 +8,6 @@
 </template>
 
 <style scoped>
-div{
-	transform: translateX(50vw);
-}
 #c{
 	font-size: 0.9em;
 }
